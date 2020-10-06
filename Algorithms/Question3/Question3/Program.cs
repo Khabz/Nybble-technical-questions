b8ff74b19799d652d51120ea45dd2472b34687b8
khabubundivhu <khabubundivh@gmail.com>
2020-10-07 01:30:49 +0200

    Question 3 completed :fire:

---
+++ Algorithms/Question3/Question3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var J = "z";
+            var S = "ZZ";
+            var output = NumJewelsInStones(J, S);
+
+            Console.Write(output);
+            Console.ReadLine();
+        }
+        static int NumJewelsInStones(string J, string S)
+        {
+            return S.Count(x => J.Contains(x));
+        }
+    }
+}
