0628c00f94da24f1a809d4e45e2fae2d034a41a8
khabubundivhu <khabubundivh@gmail.com>
2020-10-07 00:30:59 +0200

    Algorithms Question one completed

---
+++ Algorithms/Question1/Question1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 3, 1, 2, 10, 1 };
+            var output = RunningSum(input);
+            foreach (var number in output)
+            {
+                Console.Write($"{number}, ");
+            }
+            Console.ReadLine();
+        }
+
+        static int[] RunningSum(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] += numbers[i - 1];
+            }
+            return numbers;
+        }
+    }
+}
